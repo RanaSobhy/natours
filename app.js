0b98ef3c023a2d93abb1a92381c586f3b499423a
Rana Sobhy <ranasobhy1212@gmail.com>
2023-05-01 16:28:52 +0300

    Add CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,8 @@ app.set('views', path.join(__dirname, 'views'));
 //serving static files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 //set security http headers
 app.use(
